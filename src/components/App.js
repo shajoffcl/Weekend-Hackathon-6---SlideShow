fc6606d0db50594afaca91e0ed6777d6a7bccdc9
Mohd Shaib <shaj.native@gmail.com>
2020-11-22 13:48:05 +0530

    bug fixed

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,21 @@ const App = (props) => {
   const handleClick = (action) => {
     switch (action) {
       case "prev":
+        if (index === 0) {
+          return;
+        }
         setIndex(index - 1);
         break;
       case "Reset":
+        if (index === 0) {
+          return;
+        }
         setIndex(0);
         break;
       case "Next":
+        if (index === slides.length - 1) {
+          return;
+        }
         setIndex(index + 1);
         break;
       default:
@@ -23,23 +32,27 @@ const App = (props) => {
     <>
       <h1 data-testid="title">{slides[index].title}</h1>
       <p data-testid="text">{slides[index].text}</p>
-      {index !== 0 ? (
-        <button data-testid="button-prev" onClick={() => handleClick("prev")}>
-          Prev
-        </button>
-      ) : (
-        ""
-      )}
-      <button data-testid="button-restart" onClick={() => handleClick("Reset")}>
+      <button
+        data-testid="button-prev"
+        onClick={() => handleClick("prev")}
+        disabled={index === 0}
+      >
+        Prev
+      </button>
+      <button
+        data-testid="button-restart"
+        onClick={() => handleClick("Reset")}
+        disabled={index === 0}
+      >
         Reset
       </button>
-      {index !== slides.length - 1 ? (
-        <button data-testid="button-next" onClick={() => handleClick("Next")}>
-          Next
-        </button>
-      ) : (
-        ""
-      )}
+      <button
+        data-testid="button-next"
+        onClick={() => handleClick("Next")}
+        disabled={index === slides.length - 1}
+      >
+        Next
+      </button>
     </>
   );
 };
