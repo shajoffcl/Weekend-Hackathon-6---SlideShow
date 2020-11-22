32062132c9610ad54041eaba50e3d458beb6ee08
Mohd Shaib <shaj.native@gmail.com>
2020-11-22 13:35:37 +0530

    final changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,43 @@ import "../styles/App.css";
 
 const App = (props) => {
   const { slides } = props;
+  const [index, setIndex] = useState(0);
+  const handleClick = (action) => {
+    switch (action) {
+      case "prev":
+        setIndex(index - 1);
+        break;
+      case "Reset":
+        setIndex(0);
+        break;
+      case "Next":
+        setIndex(index + 1);
+        break;
+      default:
+        return;
+    }
+  };
   return (
     <>
-      {slides.map((slide) => (
-        <div>
-          <h1 data-testid="title">{slide.title}</h1>
-          <p data-testid="text">{slide.text}</p>
-        </div>
-      ))}
+      <h1 data-testid="title">{slides[index].title}</h1>
+      <p data-testid="text">{slides[index].text}</p>
+      {index !== 0 ? (
+        <button data-testid="button-prev" onClick={() => handleClick("prev")}>
+          Prev
+        </button>
+      ) : (
+        ""
+      )}
+      <button data-testid="button-restart" onClick={() => handleClick("Reset")}>
+        Reset
+      </button>
+      {index !== slides.length - 1 ? (
+        <button data-testid="button-next" onClick={() => handleClick("Next")}>
+          Next
+        </button>
+      ) : (
+        ""
+      )}
     </>
   );
 };
