93ae6772f87c82123dce1cae11bed9fe9a43b292
Mohd Shaib <shaj.native@gmail.com>
2020-11-22 13:10:24 +0530

    initial changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
+  const { slides } = props;
   return (
-    <></>
-  )
-}
-
+    <>
+      {slides.map((slide) => (
+        <div>
+          <h1 data-testid="title">{slide.title}</h1>
+          <p data-testid="text">{slide.text}</p>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default App;
